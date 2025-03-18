edc7614b2411f3215f8d2200a092718fcccf3948
Sirena-byte <ionyanova@mail.ru>
2025-03-18 22:35:44 +0300

    исправлена работа с выводом записей в требуемом кол-ве, кол-во записей всего отображается корректно, кнопки для перехода по страницам отображаются корректно(сам переход пока не работает)

--- public/model/m_allOrganizations.php
+++ public/model/m_allOrganizations.php
@@ -1,28 +1,33 @@
 <?php
 require_once("core/c_organization.php");
+
+$countPage = [10, 20, 30, 60];
 //$organizations = getOrganizations(1, 25);
-$count = 25;
 $start = 1;
 $finish = 25;
+$count = $_POST['recordsCount'] ?? 25;
 $organizations = getOrganizations(1, 25);
 $countAllOrganization = getCountOrganizations();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if (isset($_POST['myButton'])) {
-		switch ($_POST['myButton']) {
-			case "10":
-				$finish = 10;
-				break;
-			case "20":
-				$finish = 20;
-				break;
-			case "30":
-				$finish = 30;
-				break;
-			case "40":
-				$finish = 40;
-				break;
+	if (isset($_POST['recordsCount'])) {
+		if ((int)$_POST['recordsCount'] > (int)$countAllOrganization) {
+			$count = $countAllOrganization;
+		} else {
+			switch ($_POST['recordsCount']) {
+				case "$countPage[0]":
+					$finish = 10 ?? $countAllOrganization;
+					break;
+				case "20":
+					$finish = 20 ?? $countAllOrganization;
+					break;
+				case "30":
+					$finish = 30 ?? $countAllOrganization;
+					break;
+				case "60":
+					$finish = 60 ?? $countAllOrganization;
+					break;
+			}
 		}
 	}
-	$organizations = getOrganizations($start,$finish);
+	$organizations = getOrganizations($start, $finish);
 }
-$count = $_POST['myButton'];
--- public/pages/organization.php
+++ public/pages/organization.php
@@ -8,10 +8,10 @@
 				<th>Статус активности</th>
 			</tr>
 		</thead>
-		<tbody>		
-<?php require_once("model/m_allOrganizations.php");
+		<tbody>
+			<?php require_once("model/m_allOrganizations.php");
 
-$number = 1; //порядковый номер для таблицы
+			$number = 1; //порядковый номер для таблицы
 			foreach ($organizations as $organization): ?>
 				<tr>
 					<td><?= $number ?></td>
@@ -35,34 +35,44 @@ $number = 1; //порядковый номер для таблицы
 	<div class="footer-content">
 		<div class="footer-count-entries">
 			<div class="form">Записей на странице</div>
-		<form method="POST" action="">
-        <ul class="content-selection">
-            <li class="selection-item start"><button type="submit" name="myButton" value="10">10</button></li>
-            <li class="selection-item"><button type="submit" name="myButton" value="20">20</button></li>
-            <li class="selection-item"><button type="submit" name="myButton" value="30">30</button></li>
-            <li class="selection-item"><button type="submit" name="myButton" value="40">40</button></li>
-        </ul>
-    </form>
+			<form method="POST" action="">
+				<ul class="content-selection">
+
+					<?php foreach ($countPage as $item): ?>
+						<li class="selection-item"><button type="submit" name="recordsCount" value="<?= $item; ?>"><?= $item; ?></button></li>
+					<?php endforeach; ?>
+
+				</ul>
+			</form>
 		</div>
 		<div class="information">
 			<p>Записей на странице <?= $count; ?> из <?= $countAllOrganization; ?> </p>
+			<p><?php var_dump($_POST) ?></p>
 		</div>
 		<div class="footer-navigation">
 			<div class="nav-content">
-				<div class="item-nav"><img src="template/icon/footer/full-left.png" alt=""></div>
-				<div class="item-nav"><img src="template/icon/footer/left.png" alt=""></div>
-				<div class="item-nav">
-					<div class="nav-page">
-					.....
+				<form action="" method="POST">
+					<div class="item-nav"><button type="submit" name="arrow" value="fullprew"><img src="template/icon/footer/full-left.png" alt=""></button></div>
+					<div class="item-nav"><button type="submit" name="arrow" value="prew"><img src="template/icon/footer/left.png" alt=""></button></div>
+					<div class="item-nav">
+						<div class="nav-page">
+							<?php for ($i = 1, $limit = 0, $j = 1; $i <= $countAllOrganization; $i++, $limit++) {
+								if ($limit == $finish) {
+									echo "<button type=\"submit\" name=\"pageGroup\" value=\"$j\">$j</button>";
+									$j++;
+									$limit = 1;
+								} elseif ($i == $countAllOrganization) {
+									echo "<button type=\"submit\" name=\"pageGroup\" value=\"$j\">$j</button>";
+								}
+							}
+							?>
+						</div>
 					</div>
-				</div>
-				<div class="item-nav"><img src="template/icon/footer/right.png" alt=""></div>
-				<div class="item-nav"><img src="template/icon/footer/full-right.png" alt=""></div>
+					<div class="item-nav"><button type="submit" name="arrow" value="next"><img src="template/icon/footer/right.png" alt=""></button></div>
+					<div class="item-nav"><button type="submit" name="arrow" value="fullnext"><img src="template/icon/footer/full-right.png" alt=""></button></div>
+				</form>
 			</div>
 		</div>
 	</div>
 
 </div>
-<?php
-
-?>
